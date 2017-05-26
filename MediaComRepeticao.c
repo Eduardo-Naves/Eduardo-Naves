ee443d6f8ec26843caaf6f6a13ad8a6dc5d092d9
Eduardo-Naves <dudunavesc@gmail.com>
2017-05-25 22:38:22 -0300

    Add files via upload

---
+++ MediaComRepeticao.c
@@ -0,0 +1,17 @@
+int n1,n2,n3,m;
+
+int main(){
+printf("Entre a nota 1: ");
+do scanf("%d", &n1); 
+while(n1<0 || n1>10);
+
+printf("Entre a nota 2: ");
+do scanf("%d", &n2); 
+while(n2<0 || n2>10);
+
+printf("Entre a nota 2: ");
+do scanf("%d", &n3); 
+while(n3<0 || n3>10);
+
+m = (n1 + n2 + n3) / 3;
+}
